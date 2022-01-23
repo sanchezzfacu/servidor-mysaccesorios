b70bc3a752b8864817258057efcd7b1773daebf1
Facundo Sanchez <sanchezfacundocristian@hotmail.com>
2022-01-22 21:52:10 -0300

    .

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT || 3000, () => {
+  server.listen(process.env.PORT, () => {
     console.log('--------------------% listening at 3001 %--------------------'); // eslint-disable-line no-console
   });
 });
